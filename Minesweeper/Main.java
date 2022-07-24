ed494b06242c1a397982e9bb385cb42c9c6bf7c0
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2022-07-24 12:08:19 +0900

    既存ファイルを更新

--- Minesweeper/Main.java
+++ Minesweeper/Main.java
@@ -12,184 +12,195 @@ import java.awt.Dialog;
 import java.awt.Font;
 
 public class Main extends Frame implements WindowListener, MineSweeperGUI {
-  private MineSweeper ms;
-  private Button[][] tileTable;
-  private static final Font f = new Font("serif", Font.BOLD, 16);
-  private final ResultDialog resultDialog = new ResultDialog(this, "Result");
 
-  public Main() {
-    super("MainSweeper");
-    ms = new MineSweeper(9, 9, 10);   // PanelSize:9*9, Mines:10
-    init();
-  }
+    private MineSweeper ms;
+    private Button[][] tileTable;
+    private static final Font f = new Font("serif", Font.BOLD, 16);
+    private final ResultDialog resultDialog = new ResultDialog(this, "Result");
 
-  public static void main(String[] args) {
-    new Main();
-  }
-
-  private void init() {
-    this.tileTable = new Button[ms.getHeight()][ms.getWidth()];
-    this.addWindowListener(this);
-    this.setLayout(new GridLayout(ms.getHeight(), ms.getWidth()));
-    for (int i = 0; i < ms.getHeight(); i++) {
-      for (int j = 0; j < ms.getWidth(); j++) {
-        Button tile = new Button();
-        tile.setBackground(Color.LIGHT_GRAY);
-        tile.setFont(f);
-        tile.addMouseListener(new MouseEventHandler(ms, this, j, i));
-        tileTable[i][j] = tile;
-        this.add(tile);
-      }
+    public Main() {
+        super("MineSweeper");
+        ms = new MineSweeper(9, 9, 10); // 地雷が10個ある9×9の盤面
+        init();
     }
-    this.setSize(50 * ms.getWidth(), 50 * ms.getHeight());
-    this.setVisible(true);
-  }
 
-  @Override
-  public void windowOpened(WindowEvent e) {
-  }
+    public static void main(String[] args) {
+        new Main();
+    }
 
-  @Override
-  public void windowClosing(WindowEvent e) {
-    System.exit(0);
-  }
+    private void init() {
+        this.tileTable = new Button[ms.getHeight()][ms.getWidth()];
+        this.addWindowListener(this);
+        this.setLayout(new GridLayout(ms.getHeight(), ms.getWidth()));
+        for (int i = 0; i < ms.getHeight(); i++) {
+            for (int j = 0; j < ms.getWidth(); j++) {
+                Button tile = new Button();
+                tile.setBackground(Color.LIGHT_GRAY);
+                tile.setFont(f);
+                tile.addMouseListener(new MouseEventHandler(ms, this, j, i));
+                tileTable[i][j] = tile;
+                this.add(tile);
+            }
+        }
+        this.setSize(50 * ms.getWidth(), 50 * ms.getHeight());
+        this.setVisible(true);
+    }
 
-  @Override
-  public void windowClosed(WindowEvent e) {
-  }
+    @Override
+    public void windowOpened(WindowEvent e) {
 
-  @Override
-  public void windowIconified(WindowEvent e) {
-  }
+    }
 
-  @Override
-  public void windowDeiconified(WindowEvent e) {
-  }
+    @Override
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
 
-  @Override
-  public void windowActivated(WindowEvent e) {
-  }
+    @Override
+    public void windowClosed(WindowEvent e) {
 
-  @Override
-  public void windowDeactivated(WindowEvent e) {
-  }
+    }
 
-  @Override
-  public void setTextToTile(int x, int y, String text) {
-    this.tileTable[y][x].setLabel(text);
-  }
+    @Override
+    public void windowIconified(WindowEvent e) {
 
-  @Override
-  public void win() {
-    resultDialog.showDialog("Win !!!");
-  }
+    }
 
-  @Override
-  public void lose() {
-    resultDialog.showDialog("Lose ...");
-  }
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void setTextToTile(int x, int y, String text) {
+        this.tileTable[y][x].setLabel(text);
+    }
+
+    @Override
+    public void win() {
+        resultDialog.showDialog("Win !!!");
+    }
+
+    @Override
+    public void lose() {
+        resultDialog.showDialog("Lose ...");
+    }
 }
 
 class MouseEventHandler implements MouseListener {
-  MineSweeper ms;
-  MineSweeperGUI msgui;
-  int x, y;
 
-  MouseEventHandler(MineSweeper ms, MineSweeperGUI msgui, int x, int y) {
-    this.ms = ms;
-    this.msgui = msgui;
-    this.x = x;
-    this.y = y;
-  }
+    MineSweeper ms;
+    MineSweeperGUI msgui;
+    int x, y;
 
-  @Override
-  public void mouseClicked(MouseEvent e) {
-    switch (e.getButton()) {
-      case MouseEvent.BUTTON1: {
-        // Laft click
-        ms.openTile(x, y, msgui);
-      }
-      break;
-
-      case MouseEvent.BUTTON2: {
-        // Wheel click
-      }
-      break;
-
-      case MouseEvent.BUTTON3: {
-        // Right click
-        ms.setFlag(x, y, msgui);
-      }
-      break;
+    MouseEventHandler(MineSweeper ms, MineSweeperGUI msgui, int x, int y) {
+        this.ms = ms;
+        this.msgui = msgui;
+        this.x = x;
+        this.y = y;
     }
-  }
 
-  @Override
-  public void mousePressed(MouseEvent e) {
-  }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        switch (e.getButton()) {
+            case MouseEvent.BUTTON1: {
+                // Left click
+                ms.openTile(x, y, msgui);
+            }
+                break;
+            case MouseEvent.BUTTON2: {
+                // Wheel click
+            }
+                break;
+            case MouseEvent.BUTTON3: {
+                // Right click
+                ms.setFlag(x, y, msgui);
+            }
+                break;
+        }
+    }
 
-  @Override
-  public void mouseReleased(MouseEvent e) {
-  }
+    @Override
+    public void mousePressed(MouseEvent e) {
 
-  @Override
-  public void mouseEntered(MouseEvent e) {
-  }
+    }
 
-  @Override
-  public void mouseExited(MouseEvent e) {
-  }
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
 }
 
 class ResultDialog extends Dialog {
-  Label label;
-  Button btn;
 
-  public ResultDialog(Frame owner, String title) {
-    super(owner, title);
-    setLayout(new GridLayout(2, 1));
-    label = new Label();
+    Label label;
+    Button btn;
 
-    Panel p1 = new Panel();
-    p1.add(label);
-    this.add(p1);
+    public ResultDialog(Frame owner, String title) {
+        super(owner, title);
+        setLayout(new GridLayout(2, 1));
+        Panel p1 = new Panel();
+        label = new Label();
+        p1.add(label);
+        this.add(p1);
+        this.setSize(200, 100);
+        btn = new Button();
+        btn.setLabel("exit");
+        btn.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.exit(0);
+            }
 
-    this.setSize(200, 100);
+            @Override
+            public void mousePressed(MouseEvent e) {
 
-    btn = new Button();
-    btn.setLabel("exit");
+            }
 
-    btn.addMouseListener(new MouseListener() {
-      @Override
-      public void mouseClicked(MouseEvent e) {
-        System.exit(0);
-      }
+            @Override
+            public void mouseReleased(MouseEvent e) {
 
-      @Override
-      public void mousePressed(MouseEvent e) {
-      }
+            }
 
-      @Override
-      public void mouseReleased(MouseEvent e) {
-      }
+            @Override
+            public void mouseEntered(MouseEvent e) {
 
-      @Override
-      public void mouseEntered(MouseEvent e) {
-      }
+            }
 
-      @Override
-      public void mouseExited(MouseEvent e) {
-      }
-    });
+            @Override
+            public void mouseExited(MouseEvent e) {
 
-    Panel p2 = new Panel();
-    p2.add(btn);
-    this.add(p2);
-  }
+            }
+        });
+        Panel p2 = new Panel();
+        p2.add(btn);
+        this.add(p2);
+    }
 
-  public void showDialog(String message) {
-    Panel p1 = new Panel();
-    this.label.setText(message);
-    this.setVisible(true);
-  }
+    public void showDialog(String message) {
+        Panel p1 = new Panel();
+        this.label.setText(message);
+        this.setVisible(true);
+    }
 }
--- Minesweeper/MineSweeper.java
+++ Minesweeper/MineSweeper.java
@@ -1,53 +1,239 @@
+import java.util.Random;
+
+import jdk.dynalink.linker.GuardedInvocation;
+
 interface MineSweeperGUI {
-  public void setTextToTile(int x, int y, String text);
-  public void win();
-  public void lose();
+    public void setTextToTile(int x, int y, String text);
+
+    public void win();
+
+    public void lose();
 }
 
 public class MineSweeper {
-  private final int height;
-  private final int width;
-  private final int numTile;
-  private final int numMine;
-  private final int[][] table;
 
-  public MineSweeper(int height, int width, int numMine) {
-    this.height = height;
-    this.width = width;
-    this.numTile = height * width;
-    this.numMine = numMine;
-    this.table = new int [height][width];
+    private final int height;
+    private final int width;
+    private final int numberOfTiles;
+    private final int numberOfBombs;
+    private final int[][] table;
+    private int RemainTile;
 
-    initTable();
-  }
+    public MineSweeper(int height, int width, int numberOfBombs) {
+        this.height = height;
+        this.width = width;
+        this.numberOfTiles = height * width;
+        this.numberOfBombs = numberOfBombs;
+        this.table = new int[height][width];
+        this.RemainTile = numberOfTiles - numberOfBombs;
 
-  public int getHeight() {
-    return height;
-  }
+        initTable();
+    }
 
-  public int getWidth() {
-    return width;
-  }
+    public int getHeight() {
+        return height;
+    }
 
-  void initTable() {
-    setMine();
+    public int getWidth() {
+        return width;
+    }
 
-    /* ----- add implementation here ----- */
-  }
+    void initTable() {
+        setBombs();
 
-  void setMine() {
-    /* ----- add implementation here ----- */
-  }
+        /* ----- ここから実装してください． ----- */
+    }
 
-  public void openTile(int x, int y, MineSweeperGUI gui) {
-    /* ----- add implementation here ----- */
-  }
+    void setBombs() {
+        /* ----- ここから実装してください． ----- */
+        Random ranndom = new Random();
+        for (int i = 0; i < this.numberOfBombs; i++) {
+            int y = ranndom.nextInt(this.height);
+            int x = ranndom.nextInt(this.width);
+            if (this.checkBombs(x, y) == false) {
+                i--;
+                continue;
+            }
+            this.table[y][x] = -1;
+            System.out.println(y + " " + x);
+        }
+    }
 
-  public void setFlag(int x, int y, MineSweeperGUI gui) {
-    /* ----- add implementation here ----- */
-  }
+    public void openTile(int x, int y, MineSweeperGUI gui) {
+        /* ----- ここから実装してください． ----- */
+        if (this.table[y][x] <= 9) {
+            if (this.checkBombs(x, y) == false) {
+                gui.lose();
+                this.openAllTiles(gui);
+            }
+            int count = 0;
+            // y>0の時
+            if (y != 0) {
+                if (checkBombs(x, y - 1) == false) {
+                    count++;
+                }
+                if (x != 0) {
+                    if (checkBombs(x - 1, y) == false) {
+                        count++;
+                    }
+                    if (checkBombs(x - 1, y - 1) == false) {
+                        count++;
+                    }
+                }
+                if (x != width - 1) {
+                    if (checkBombs(x + 1, y) == false) {
+                        count++;
+                    }
+                    if (checkBombs(x + 1, y - 1) == false) {
+                        count++;
+                    }
+                }
+                if (y != height - 1) {
+                    if (checkBombs(x, y + 1) == false) {
+                        count++;
+                    }
+                    if (x != 0) {
+                        if (checkBombs(x - 1, y + 1) == false) {
+                            count++;
+                        }
+                    }
+                    if (x != width - 1) {
+                        if (checkBombs(x + 1, y + 1) == false) {
+                            count++;
+                        }
+                    }
+                }
+            }
+            // y=0の時
+            if (y == 0) {
+                if (x != 0) {
+                    if (checkBombs(x - 1, y) == false) {
+                        count++;
+                    }
+                }
+                if (x != width - 1) {
+                    if (checkBombs(x + 1, y) == false) {
+                        count++;
+                    }
+                }
+                if (checkBombs(x, y + 1) == false) {
+                    count++;
+                }
+                if (x != 0) {
+                    if (checkBombs(x - 1, y + 1) == false) {
+                        count++;
+                    }
+                }
+                if (x != width - 1) {
+                    if (checkBombs(x + 1, y + 1) == false) {
+                        count++;
+                    }
+                }
+            }
+            String Count = Integer.toString(count);
+            gui.setTextToTile(x, y, Count);
+        }
+        this.RemainTile--;
+        if (this.RemainTile == 0) {
+            gui.win();
+        }
+    }
 
-  public void openAllTiles(MineSweeperGUI gui) {
-    /* ----- add implementation here ----- */
-  }
+    public void setFlag(int x, int y, MineSweeperGUI gui) {
+        /* ----- ここから実装してください． ----- */
+        if (this.table[y][x] >= 9) {
+            gui.setTextToTile(x, y, null);
+            this.table[y][x] -= 10;
+        } else {
+            this.table[y][x] += 10;
+            gui.setTextToTile(x, y, "F");
+        }
+    }
+
+    private void openAllTiles(MineSweeperGUI gui) {
+        /* ----- ここから実装してください． ----- */
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (this.table[y][x] == -1) {
+                    gui.setTextToTile(x, y, "-1");
+                } else {
+                    int count = 0;
+                    // y<0の時
+                    if (y != 0) {
+                        if (checkBombs(x, y - 1) == false) {
+                            count++;
+                        }
+                        if (x != 0) {
+                            if (checkBombs(x - 1, y) == false) {
+                                count++;
+                            }
+                            if (checkBombs(x - 1, y - 1) == false) {
+                                count++;
+                            }
+                        }
+                        if (x != width - 1) {
+                            if (checkBombs(x + 1, y) == false) {
+                                count++;
+                            }
+                            if (checkBombs(x + 1, y - 1) == false) {
+                                count++;
+                            }
+                        }
+                        if (y != height - 1) {
+                            if (checkBombs(x, y + 1) == false) {
+                                count++;
+                            }
+                            if (x != 0) {
+                                if (checkBombs(x - 1, y + 1) == false) {
+                                    count++;
+                                }
+                            }
+                            if (x != width - 1) {
+                                if (checkBombs(x + 1, y + 1) == false) {
+                                    count++;
+                                }
+                            }
+                        }
+                    }
+                    // y=0の時
+                    if (y == 0) {
+                        if (x != 0) {
+                            if (checkBombs(x - 1, y) == false) {
+                                count++;
+                            }
+                        }
+                        if (x != width - 1) {
+                            if (checkBombs(x + 1, y) == false) {
+                                count++;
+                            }
+                        }
+                        if (checkBombs(x, y + 1) == false) {
+                            count++;
+                        }
+                        if (x != 0) {
+                            if (checkBombs(x - 1, y + 1) == false) {
+                                count++;
+                            }
+                        }
+                        if (x != width - 1) {
+                            if (checkBombs(x + 1, y + 1) == false) {
+                                count++;
+                            }
+                        }
+                    }
+                    String Count = Integer.toString(count);
+                    gui.setTextToTile(x, y, Count);
+                }
+            }
+        }
+    }
+
+    // 指定されたタイルに爆弾があればfalseを返す
+    public boolean checkBombs(int x, int y) {
+        if (this.table[y][x] == -1) {
+            return false;
+        }
+        return true;
+    }
+
 }
