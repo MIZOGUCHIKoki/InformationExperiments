23da65f153e96e101472d469f4dc1882d6c4cfba
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2022-07-28 14:06:56 +0900

    CHANGED : Color settings

--- Minesweeper/Main.java
+++ Minesweeper/Main.java
@@ -61,19 +61,15 @@ public class Main extends Frame implements WindowListener, MineSweeperGUI {
         switch (num) {
             case 1:
                 this.tileTable[y][x].setForeground(Color.blue);
-                this.setGray(x, y);
                 break;
             case 2:
                 this.tileTable[y][x].setForeground(new Color(0, 125, 0));
-                this.setGray(x, y);
                 break;
             case 3:
                 this.tileTable[y][x].setForeground(Color.red);
-                this.setGray(x, y);
                 break;
             default:
                 this.tileTable[y][x].setForeground(Color.black);
-                this.setGray(x, y);
         }
     }
 
