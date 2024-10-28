6f1ab0b49fdfe11dcab3014935d93fb51499e6e6
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2024-10-28 15:18:25 +0900

    Update

--- ComputerSystem/Assignment4/mizoguchi-1250373-D1.c
+++ ComputerSystem/Assignment4/mizoguchi-1250373-D1.c
@@ -2,26 +2,32 @@
 #include <string.h> // memset()
 #include <unistd.h> // stdin
 #include <pthread.h>
+#include <semaphore.h>
+#include <fcntl.h> // For O_* constants
 
 void *func(void *param);
-int flag = 0;
 char buf[256];
+const char* semName = "/sem";
+sem_t *sem;
 
 int main() {
 	memset(buf, ' ', 256);
-
 	pthread_t tid;
 	pthread_create(&tid, NULL, func, NULL);
 
+	sem  = sem_open(semName, O_CREAT, 0666 , 1);
+	sem_wait(sem); // wait()
 	printf("\nParents Process: > ");
 	fgets(buf, 256, stdin);
-	flag = 1;
+	sem_post(sem); // Signal()
 	pthread_join(tid, NULL);
+	sem_close(sem);
 	return 0;
 }
 
 void *func(void *param) {
-	while (flag == 0);
+	sem_wait(sem); // wait()
 	printf("\nThread: > %s\n", buf);
+	sem_post(sem); // Signal()
 	pthread_exit(0);
 }
